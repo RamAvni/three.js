5f3c4f1079b1ea32829f6b9b27cd895708e534f4
RamAvni <ramavni@hilma.tech>
2025-02-07 20:47:03 +0200

    forgot what these are

--- freeCodeCamp-three.js-5-projects/first/src/main.ts
+++ freeCodeCamp-three.js-5-projects/first/src/main.ts
@@ -1,24 +1,39 @@
-import './style.css'
-import typescriptLogo from './typescript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.ts'
+import * as THREE from "three";
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-  </div>
-`
+const w = window.innerWidth;
+const h = window.innerHeight;
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+function threeCanvasSetup() {
+    const renderer = new THREE.WebGLRenderer();
+    renderer.setSize(w, h);
+
+    document.body.appendChild(renderer.domElement);
+    return renderer;
+}
+
+function threeCameraSetup() {
+    const fov = 75;
+    const aspect = w / h;
+    const near = 0.1;
+    const far = 10;
+
+    const camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
+    camera.position.z = 2;
+
+    return camera;
+}
+
+function threeSetup() {
+    const renderer = threeCanvasSetup();
+    const camera = threeCameraSetup();
+    const scene = new THREE.Scene(); // Scene Setup
+
+    return { renderer, camera, scene };
+}
+
+function main() {
+    const world = threeSetup();
+    world.renderer.render(world.scene, world.camera);
+}
+
+main();
