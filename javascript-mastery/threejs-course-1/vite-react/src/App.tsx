b7aa1cda3322c6e708e19552f3fa9ee6b5633459
RamAvni <ramavni@hilma.tech>
2025-02-08 15:55:30 +0200

    everything

--- javascript-mastery/threejs-course-1/vite-react/src/App.tsx
+++ javascript-mastery/threejs-course-1/vite-react/src/App.tsx
@@ -1,6 +1,44 @@
-import { FC } from "react";
+import { OrbitControls } from "@react-three/drei";
+import { Canvas, useFrame } from "@react-three/fiber";
+import { FC, useRef } from "react";
+import { Mesh } from "three";
+
+const RoatatingCube = () => {
+  const meshRef = useRef<Mesh>(null);
+  useFrame(() => {
+    if (meshRef.current) {
+      meshRef.current.rotation.x += 0.01;
+      meshRef.current.rotation.y += 0.01;
+    }
+  });
+  return (
+    <mesh ref={meshRef}>
+      <boxGeometry />
+      <meshLambertMaterial color="#468585" emissive="#468585" />
+    </mesh>
+  );
+};
 
 const App: FC = () => {
-  return <div>hello three.js</div>;
+  return (
+    <Canvas
+      style={{
+        height: "100vh",
+        width: "100vw",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <OrbitControls enableZoom enablePan enableRotate />
+      <directionalLight
+        position={[1, 1, 1]}
+        intensity={10}
+        color={"0x0CDBA6"}
+      />
+      <color attach="background" args={["#F0F0F0"]} />
+      <RoatatingCube />
+    </Canvas>
+  );
 };
 export default App;
