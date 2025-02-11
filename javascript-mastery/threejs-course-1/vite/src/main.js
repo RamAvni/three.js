7d90383a2d22d8db3c00b363acf49dd8310e0730
RamAvni <ramavni@hilma.tech>
2025-02-11 11:38:53 +0200

    hi

--- javascript-mastery/threejs-course-1/vite/src/main.js
+++ javascript-mastery/threejs-course-1/vite/src/main.js
@@ -1,1 +1,15 @@
+import * as THREE from "three";
 
+// 1. Create the scene
+const scene = new THREE.Scene();
+scene.background = new THREE.Color("#FFF");
+
+// 2. Add the camerad
+
+// 3. Create and add a cube object
+
+// 4. Add lighting
+
+// 5. Set up the renderer
+
+// 6. Animate the scene
